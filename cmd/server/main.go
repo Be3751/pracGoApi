8627c629ec824db922768771a3114aeca53a7be8
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-03-05 22:48:31 +0900

    developped a simple api

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-	"os"
 
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprint(rw, "Hello, world!")
-	})
+// タグによりJSONにシリアライズ
+// タグがないと，フィールド名がそのまま用いられる
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
 
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+// ginのContextはリクエストの詳細や認証，シリアライズされたJSONを渡す
+func getAlbums(ctx *gin.Context) {
+	ctx.IndentedJSON(http.StatusOK, albums) // ステータスコード200で構造体albumsをJSONにシリアライズ
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums) // エンドポイントとハンドラーをマッピング（渡しているのはハンドラーであってハンドラー関数の結果ではない点に注意）
+	router.Run("localhost:8080")
 }
